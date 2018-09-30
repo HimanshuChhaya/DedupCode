394b86d473fbcdbac5f63b69a8b7791a94dac5d8
2016ht13336 <2016ht13336@wilp.bits-pilani.ac.in>
2018-09-30 20:17:55 +0530

    first commit

---
+++ Dedup/Dedup/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using Microsoft.Win32;
+
+namespace Dedup
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelect_Click(object sender, RoutedEventArgs e)
+        {
+            TreeViewForm form = new TreeViewForm();
+            form.Owner = this;
+            form.ShowDialog();
+        }
+    }
+}
---
+++ Dedup/Dedup/TreeViewForm.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Dedup
+{
+    /// <summary>
+    /// Interaction logic for TreeViewForm.xaml
+    /// </summary>
+    public partial class TreeViewForm : Window
+    {
+        public TreeViewForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach(DriveInfo info in System.IO.DriveInfo.GetDrives())
+            {
+                //TreeViewItem item = new TreeViewItem();
+                //item.Header = info.Name;
+                //treeView.Items.Add(item);
+                treeView.Items.Add(CreateTreeItem(info));
+            }
+        }
+
+        private void treeView_Expanded(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = e.Source as TreeViewItem;
+            if ((item.Items.Count == 1) && (item.Items[0] is string))
+            {
+                item.Items.Clear();
+
+                DirectoryInfo expandedDir = null;
+                if (item.Tag is DriveInfo)
+                    expandedDir = (item.Tag as DriveInfo).RootDirectory;
+                if (item.Tag is DirectoryInfo)
+                    expandedDir = (item.Tag as DirectoryInfo);
+                try
+                {
+                    foreach (DirectoryInfo subDir in expandedDir.GetDirectories())
+                        item.Items.Add(CreateTreeItem(subDir));
+
+                    foreach(FileInfo files in expandedDir.GetFiles())
+                        item.Items.Add(CreateTreeItem(files));
+                }
+                catch { }
+            }
+        }
+
+        private TreeViewItem CreateTreeItem(object o)
+        {
+            TreeViewItem item = new TreeViewItem();
+
+            item.Header = o.ToString();
+            item.Tag = o;
+            item.Items.Add("Loading...");
+            return item;
+        }
+    }
+}
